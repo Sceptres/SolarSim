aa2bc070762a498b171d0a83f546443ceed2b050
Abdallah Alqashqish <abdalq2003@gmail.com>
2024-09-28 10:51:56 -0400

    Implement camera looking and moving

--- camera/Camera.cpp
+++ camera/Camera.cpp
@@ -2,14 +2,42 @@
 
 Camera::Camera(const glm::vec3 startPos, GLfloat fov, GLfloat aspectRatio, GLfloat nearPlaneDistance, GLfloat farPlaneDistance) {
     this->position = glm::vec3(startPos.x, startPos.y, startPos.z);
-    this->view = glm::mat4(1.0f);
-    this->view = glm::translate(this->view, this->position);
+    this->target = glm::vec3(0);
+    this->up = glm::vec3(0);
+    this->UpdateStates(this->position, this->target);
+    this->view = glm::lookAt(this->position, this->target, this->up);
     this->projection = glm::perspective(fov, aspectRatio, nearPlaneDistance, farPlaneDistance);
 }
 
 void Camera::Apply(ShaderProgram& shaderProgram) {
     shaderProgram.setMat4("view", this->view);
     shaderProgram.setMat4("projection", this->projection);
+}
+
+void Camera::MoveTo(glm::vec3 newPosition) {
+    this->position.x = newPosition.x;
+    this->position.y = newPosition.y;
+    this->position.z = newPosition.z;
+    this->UpdateStates(this->position, this->target);
+}
+
+void Camera::LookAt(glm::vec3 target) {
+    this->UpdateStates(this->position, target);
+}
+
+void Camera::UpdateStates(glm::vec3 pos, glm::vec3 target) {
+    this->target.x = target.x;
+    this->target.y = target.y;
+    this->target.z = target.z;
+    
+    glm::vec3 camDirection = glm::normalize(this->position - this->target);
+    glm::vec3 upVec = glm::vec3(0.0f, 1.0f, 0.0f); 
+    glm::vec3 cameraRight = glm::normalize(glm::cross(upVec, camDirection));
+    glm::vec3 cameraUp = glm::cross(camDirection, cameraRight);
+    
+    this->up.x = cameraUp.x;
+    this->up.y = cameraUp.y;
+    this->up.z = cameraUp.z;
 }
 
 glm::vec3 Camera::getPosition() {
--- camera/Camera.hpp
+++ camera/Camera.hpp
@@ -12,12 +12,17 @@ class Camera {
     public:
         Camera(const glm::vec3 startPos, GLfloat fov, GLfloat aspectRatio, GLfloat nearPlaneDistance, GLfloat farPlaneDistance);
         void Apply(ShaderProgram& shaderProgram);
+        void LookAt(glm::vec3 target);
+        void MoveTo(glm::vec3 newPosition);
         glm::vec3 getPosition();
 
     private:
         glm::vec3 position;
+        glm::vec3 target;
+        glm::vec3 up;
         glm::mat4 view;
         glm::mat4 projection;
+        void UpdateStates(glm::vec3 pos, glm::vec3 target);
 };
 
 #endif
