6616d9e83815c9a4aaa00db0658da1571ba9d6f7
Abdallah Alqashqish <abdalq2003@gmail.com>
2024-10-03 14:06:00 -0400

    Implement color class

---
+++ color/Color.cpp
@@ -0,0 +1,3 @@
+#include "Color.hpp"
+
+Color::Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {}
---
+++ color/Color.hpp
@@ -0,0 +1,14 @@
+#ifndef COLOR_CLASS
+
+#define COLOR_CLASS
+
+class Color {
+    public:
+        Color(float r, float g, float b, float a);
+        const float r;
+        const float g;
+        const float b;
+        const float a;
+};
+
+#endif
